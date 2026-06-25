29298a58beace5c88442972e669285959031bfcd
Swasti-Bansal <swastijibansal2145@gmail.com>
2026-06-25 17:24:00 +0530

    Added JUnit Exercises

---
+++ Week 1/JUnit Testing/Exercise 1/CalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+```
+@Test
+public void testAddition() {
+    int result = 10 + 5;
+    assertEquals(15, result);
+}
+```
+
+}
