04f3cb192f46321e75d6e5f2a06b68206e6f6d0a
Swasti-Bansal <swastijibansal2145@gmail.com>
2026-07-08 12:42:07 +0530

    Added exercises of Week 3

---
+++ Week 3/JWT Authentication/Authentication Service/src/main/java/com/cognizant/jwt/controller/AuthenticationController.java
@@ -0,0 +1,16 @@
+package com.cognizant.jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/authenticate")
+    public String authenticate() {
+
+        return "JWT Authentication Service";
+
+    }
+
+}
