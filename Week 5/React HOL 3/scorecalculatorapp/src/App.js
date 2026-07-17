8a2b6258d0797a89bf40e127587c846033d039b5
Swasti-Bansal <swastijibansal2145@gmail.com>
2026-07-18 01:02:05 +0530

    Added Week 5 Exercises

---
+++ Week 5/React HOL 3/scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+    return (
+        <CalculateScore
+            name="Rahul"
+            school="ABC School"
+            total={450}
+            goal={5}
+        />
+    );
+}
+
+export default App;
